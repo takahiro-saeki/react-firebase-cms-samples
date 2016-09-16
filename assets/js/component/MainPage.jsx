a511fc688946c3decce30211c8f606c82751a48e
takahiro-saeki <hiroooon69@gmail.com>
2016-09-15 20:48:04 -0700

    add component.

--- assets/js/component/MainPage.jsx
+++ assets/js/component/MainPage.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Mui from '../data/mui.js';
 import Header from './Header';
+import SideMenu from './SideMenu';
+import style from '../../css/style.css';
 
 export default class Main extends Component {
   render() {
@@ -9,7 +11,14 @@ export default class Main extends Component {
       <MuiThemeProvider muiTheme={Mui}>
         <main>
           <Header title="メニュー"/>
-          <div onClick={this.props.onClickCheck}>テストです</div>
+          <SideMenu />
+          <div
+            className={style['content-wrap']}
+            onClick={this.props.onClickCheck}>
+              <section>
+                
+              </section>
+          </div>
         </main>
       </MuiThemeProvider>
     )
